5a99947c51102cd3a337ecb9835fba0394f98f51
ariannedee <ariannedee@gmail.com>
2025-06-25 16:37:12 -0700

    Add aliases

---
+++ examples/ex_5_aliases.py
@@ -0,0 +1,46 @@
+# %% Aliases
+from math import sqrt
+
+Coordinates = tuple[float, float]
+
+def distance(p1: Coordinates, p2: Coordinates) -> float:
+    return sqrt(
+        (p1[0] - p2[0]) ** 2 +
+        (p1[1] - p2[1]) ** 2
+    )
+
+print(distance((10, 10), (7, 6)))
+
+# %%
+from typing import NewType
+
+UserId = NewType("UserId", int)
+
+def get_user(user_id: UserId) -> dict:
+    ...
+
+uid1 = UserId(1)    # Works
+uid2 = UserId('2')  # Mypy: arg-type
+
+get_user(3)          # Mypy: arg-type
+get_user(UserId(4))  # Works
+
+# %%
+from typing import TypedDict, NewType
+
+UserId = NewType("UserId", int)
+ProductId = NewType("ProductId", int)
+
+class Purchase(TypedDict):
+    user_id: UserId
+    product_id: ProductId
+    quantity: int
+
+purchase: Purchase = {
+    "user_id": UserId(1),
+    "product_id": ProductId(42),
+    "quantity": 3,
+}
+
+def process(p: Purchase) -> None:
+    print(f"User {p['user_id']} bought {p['quantity']} of product {p['product_id']}")
