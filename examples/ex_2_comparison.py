35422216ed5a913974a39f6ce1fa2a1a61c00ef2
ariannedee <ariannedee@gmail.com>
2025-07-13 23:22:04 -0700

    Update example

--- examples/ex_2_comparison.py
+++ examples/ex_2_comparison.py
@@ -17,6 +17,18 @@ def is_correct_w_docstring(guess, answer):
     return guess.strip().lower() == answer.lower()
 
 
+def is_correct_w_type_comments(guess, answer):
+    # type: (str, str) -> bool
+    return guess.strip().lower() == answer.lower()
+
+
+def is_correct_w_type_comments_multiline(
+        guess,  # type: str
+        answer  # type: str
+):  # type: (...) -> bool
+    return guess.strip().lower() == answer.lower()
+
+
 def is_correct_w_annotations(guess: str, answer: str) -> bool:
     return guess.strip().lower() == answer.lower()
 
@@ -27,52 +39,88 @@ def is_correct_best_practice(guess: str, answer: str) -> bool:
     """
     return guess.strip().lower() == answer.lower()
 
-# -------------------------------------------------------------------
 
-# More complex function without type hints
-def run_quiz_basic(questions: list[dict[str, str]]) -> None:
-    for question in questions:
-        answer = input(question["question"] + "? ")
-        if answer == question["answer"]:
-            print("Correct")
-        else:
-            print("Incorrect")
+# %% More complex function without type hints
+def group_by_category(items):
+    grouped = {}
+    for item in items:
+        category = item["category"]
+        name = item["name"]
+        grouped.setdefault(category, []).append(name)
+    return grouped
 
-quiz_questions = [
-    {"question": "What is the meaning of life?", "answer": "42"},
-    {"question": "What was Python named after?", "answer": "Monty Python"}
+
+grocery_list = [
+    {"category": "Pasta", "name": "Spaghetti"},
+    {"category": "Dairy", "name": "Eggs"},
+    {"category": "Dairy", "name": "Parmesan"},
+    {"category": "Produce", "name": "Garlic"},
+    {"category": "Produce", "name": "Snap peas"},
 ]
 
-run_quiz_basic(quiz_questions)
+groceries_by_aisle = {  # Return value from group_by_category()
+    'Pasta': ['Spaghetti'],
+    'Dairy': ['Eggs', 'Parmesan'],
+    'Produce': ['Garlic', 'Snap peas']
+}
+
+assert group_by_category(grocery_list) == groceries_by_aisle
 
 
-# With built-in generic containers
-def run_quiz_w_container(questions: list[dict[str, str]]) -> None:
-    for question in questions:
-        answer = input(question["question"] + "? ")
-        if answer == question["answer"]:
-            print("Correct")
-        else:
-            print("Incorrect")
+# %% With docstring
 
-quiz_questions: list[dict[str, str]]
-run_quiz_w_container(quiz_questions)
+def group_by_category_w_docstring(items):
+    """ Group items based on their category.
+    Args:
+        items (list of dict): A list of dictionaries representing items.
+            Each dictionary must have the following structure:
+            - 'category' (str)
+            - 'name' (str)
+
+    Returns:
+        A dict mapping categories (str) to a list of item names (str).
+        For example:
+            {
+                'Language': ['Python', 'Java'],
+                'Database': ['MySQL'],
+            }
+    """
+    ...
 
 
-# With typing containers and TypedDict
-from typing import List, TypedDict
+# %% With built-in generic containers
 
-class Question(TypedDict):
-    question: str
-    answer: str
+def group_by_category_w_hints(items: list[dict[str, str]]) -> dict[str, list[str]]:
+    grouped: dict[str, list[str]] = {}
+    for item in items:
+        category = item["category"]
+        name = item["name"]
+        grouped.setdefault(category, []).append(name)
+    return grouped
 
-def run_quiz_w_typeddict(questions: List[Question]) -> None:
-    for question in questions:
-        answer = input(question["question"] + "? ")
-        if answer == question["answer"]:
-            print("Correct")
-        else:
-            print("Incorrect")
 
-quiz_questions: List[Question]
-run_quiz_w_typeddict(quiz_questions)
+grocery_list: list[dict[str, str]]
+
+group_by_category_w_hints(grocery_list)
+
+# %% With typing imports and TypedDict
+from typing import Dict, List, TypedDict
+
+
+class Item(TypedDict):
+    category: str
+    name: str
+
+
+def group_by_category_w_typed_dict(items: List[Item]) -> Dict[str, List[str]]:
+    grouped: Dict[str, List[str]] = {}
+    for item in items:
+        category = item["category"]
+        name = item["name"]
+        grouped.setdefault(category, []).append(name)
+    return grouped
+
+
+grocery_list: List[Item]
+
+group_by_category_w_typed_dict(grocery_list)
