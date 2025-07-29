3702e058f1e11d949fa6cd2499feb3af0f2babac
ariannedee <ariannedee@gmail.com>
2025-07-29 16:45:03 -0700

    Get file to run in 3.9

--- examples/ex_14_other_cases.py
+++ examples/ex_14_other_cases.py
@@ -4,34 +4,41 @@ def print_names(*args: str) -> None:
         print(name)
 
 print_names("Ringo", "John", "George", "Paul")
-print_names(1)  # arg-type error
+print_names(1)  # mypy: arg-type
 
 # %% **kwargs
-def as_dict(**kwargs: int | float) -> dict[str, int | float]:
+from typing import Union
+
+NumberOrStr = Union[float, str]
+
+def as_dict(**kwargs: NumberOrStr) -> dict[str, NumberOrStr]:
     return kwargs
 
 print(as_dict(a=1, b=2.0))
-print(as_dict(a='1'))  # arg-type error
+print(as_dict(a=["a", "list"]))  # mypy: arg-type
 
 # %% Decorators
-from typing import Callable, ParamSpec, TypeVar
+import sys
 
-P = ParamSpec('P')
-R = TypeVar('R')
+if sys.version_info >= (3, 10):
+    from typing import Callable, ParamSpec, TypeVar
 
-def log_decorator(func: Callable[P, R]) -> Callable[P, R]:
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-        result = func(*args, **kwargs)
-        print(f"Input: {args}, {kwargs}")
-        print(f"Output: {result}")
-        return result
-    return wrapper
+    P = ParamSpec('P')  # 3.10+
+    R = TypeVar('R')
 
-@log_decorator
-def add_numbers(a: int, b: int) -> int:
-    return a + b
+    def log_decorator(func: Callable[P, R]) -> Callable[P, R]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            result = func(*args, **kwargs)
+            print(f"Input: {args}, {kwargs}")
+            print(f"Output: {result}")
+            return result
+        return wrapper
 
-print(add_numbers('1', '2'))  # Mypy: arg-type
+    @log_decorator
+    def add_numbers(a: int, b: int) -> int:
+        return a + b
+
+    print(add_numbers('1', '2'))  # mypy: arg-type
 
 # %% Generators
 from typing import Generator, Iterator
@@ -45,17 +52,17 @@ def fib(n: int) -> Generator[int, None, None]:
         b, a = a + b, b
         n -= 1
 
-g: Generator[int] = fib(10)
+g: Generator[int, None, None] = fib(10)
 i: Iterator[int] = (x for x in range(3))
 
 # %% Overload
-from typing import overload
+from typing import overload, Union
 
 @overload
 def parse_value(value: str) -> int: ...
 @overload
 def parse_value(value: list[str]) -> list[int]: ...
-def parse_value(value: str | list[str]) -> int | list[int]:
+def parse_value(value: Union[str, list[str]]) -> Union[int, list[int]]:  # In 3.10+, use | instead of Union
     """
     Parse either a single string or a list of strings into integers.
     """
@@ -66,4 +73,4 @@ def parse_value(value: str | list[str]) -> int | list[int]:
 # Type checker understands these correctly
 with_int: int = parse_value("123")
 with_list: list[int] = parse_value(["123", "456"])
-should_return_list: int = parse_value(["123"])  # Type error
+should_return_list: int = parse_value(["123"])  # mypy: assignment
